82b4d15242395c692fc35e5ca775fe0017fdc439
Fernando <arayafernando22@gmail.com>
2012-04-03 13:53:28 -0600

    Test

--- MppViewer/Day.h
+++ MppViewer/Day.h
@@ -3,7 +3,7 @@
 //  mppParser
 //
 //  Created by George on 3/27/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 __MyCompanyName1__. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
