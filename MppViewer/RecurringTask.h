fe8cd85b80797d270bbd4634db0934b63bedddb2
Fernando <arayafernando22@gmail.com>
2012-04-13 18:00:09 -0600

    RecurringTask

---
+++ MppViewer/RecurringTask.h
@@ -0,0 +1,61 @@
+//
+//  RecurringTask.h
+//  MppViewer
+//
+//  Created by Fernando Araya on 4/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class MPPViewerDuration;
+@class RecurrenceType;
+@class Day;
+
+@interface RecurringTask : NSObject
+
+@property (strong, nonatomic) NSDate *StartDate;
+
+@property (strong, nonatomic) NSDate *FinishDate;
+
+@property (strong, nonatomic) MPPViewerDuration *Duration;
+
+@property (nonatomic) int Ocurrences;
+
+@property (strong, nonatomic) RecurrenceType *RecurrenceType;
+
+@property (nonatomic) Boolean UseEndDate;
+
+@property (nonatomic) Boolean DailyWorkDay;
+
+@property (nonatomic) Boolean WeeklyDays;
+
+@property (nonatomic) Boolean MonthlyRelative;
+
+@property (nonatomic) Boolean YearlyAbsolute;
+
+@property (nonatomic) int DailyFrecuency;
+
+@property (nonatomic) int WeeklyFrecuency;
+
+@property (nonatomic) int MontlyRelativeOrdinal;
+
+@property (strong, nonatomic) Day * MontlyRelativeDay;
+
+@property (nonatomic) int MontlyRelativeFrecuency;
+
+@property (nonatomic) int MontlyAbsoluteDay;
+
+@property (nonatomic) int MontlyAbsoluteFrecuency;
+
+@property (nonatomic) int YearlyRelativeOrdinal;
+
+@property (strong, nonatomic) Day * YearlyRelativeDay;
+
+@property (nonatomic) int YearlyRelativeMonth;
+
+@property (strong, nonatomic) NSDate *YearlyAbsoluteDate;
+
+-(NSString *) toString;
+
+@end
---
+++ MppViewer/RecurringTask.m
@@ -0,0 +1,41 @@
+//
+//  RecurringTask.m
+//  MppViewer
+//
+//  Created by Fernando Arayao 10 a on 4/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RecurringTask.h"
+
+@implementation RecurringTask
+
+@synthesize StartDate;
+@synthesize FinishDate;
+@synthesize Duration;
+@synthesize Ocurrences;
+@synthesize RecurrenceType;
+@synthesize UseEndDate;
+@synthesize DailyWorkDay;
+@synthesize WeeklyDays;
+@synthesize MonthlyRelative;
+@synthesize YearlyAbsolute;
+@synthesize DailyFrecuency;
+@synthesize WeeklyFrecuency;
+@synthesize MontlyRelativeOrdinal;
+@synthesize MontlyRelativeDay;
+@synthesize MontlyRelativeFrecuency;
+@synthesize MontlyAbsoluteDay;
+@synthesize MontlyAbsoluteFrecuency;
+@synthesize YearlyRelativeOrdinal;
+@synthesize YearlyRelativeDay;
+@synthesize YearlyRelativeMonth;
+@synthesize YearlyAbsoluteDate;
+
+-(NSString *) toString
+{
+    #warning Incomplete
+    return nil;
+}
+
+@end
